0043bae6d555fe1ed27ef9521a12c12551641767
gonnichiwa <keepthefaith00@gmail.com>
2024-05-27 14:57:03 +0900

    apply prettier

--- src/EventPracticeFun.js
+++ src/EventPracticeFun.js
@@ -1,45 +1,48 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const EventPracticeFun = () => {
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
 
-    const onChangeUsername = (e) => {setUsername(e.target.value) };
-    const onChangeMessage = (e) => {setMessage(e.target.value)};
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+  const onChangeMessage = (e) => {
+    setMessage(e.target.value);
+  };
 
-    const onHandleKeyUp = (e) => {
-        if(e.key === 'Enter'){
-            onHandleClick()
-        }
+  const onHandleKeyUp = (e) => {
+    if (e.key === "Enter") {
+      onHandleClick();
     }
-    const onHandleClick = () => {
-        setUsername('');
-        setMessage('');
-        alert(username + ' ' + message)};
+  };
+  const onHandleClick = () => {
+    setUsername("");
+    setMessage("");
+    alert(username + " " + message);
+  };
 
-    return (
-        <div>
-        <h1>이벤트 연습</h1>
-        <input 
-          type="text"
-          name="username"
-          placeholder="사용자명"
-          value={username}
-          onChange={onChangeUsername}
-        />
-        <input 
-          type="text"
-          name="message"
-          placeholder="아무거나입력해보시오"
-          value={message}
-          onChange={onChangeMessage}
-          onKeyUp={onHandleKeyUp}
-        />
-        <button
-          onClick={onHandleClick}
-        >확인</button>
-      </div>
-    );
-}
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChangeUsername}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나입력해보시오"
+        value={message}
+        onChange={onChangeMessage}
+        onKeyUp={onHandleKeyUp}
+      />
+      <button onClick={onHandleClick}>확인</button>
+    </div>
+  );
+};
 
 export default EventPracticeFun;
