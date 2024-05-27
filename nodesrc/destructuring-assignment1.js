a592e5caf2e1d427928da38409c99be2f7ca4647
gonnichiwa <keepthefaith00@gmail.com>
2024-05-28 08:45:09 +0900

    append example

--- nodesrc/destructuring-assignment1.js
+++ nodesrc/destructuring-assignment1.js
@@ -78,3 +78,34 @@ const info = {
 const nextInfo = {...info, };
 console.log(nextInfo); // { username: 'name', message: 'msg' }
 console.log(info);     // { username: 'name', message: 'msg' }
+
+// /////////////////////////////////////////
+
+let people = [
+    {
+      name: "Mike Smith",
+      family: {
+        mother: "Jane Smith",
+        father: "Harry Smith",
+        sister: "Samantha Smith",
+      },
+      age: 35,
+    },
+    {
+      name: "Tom Jones",
+      family: {
+        mother: "Norah Jones",
+        father: "Richard Jones",
+        brother: "Howard Jones",
+      },
+      age: 25,
+    },
+  ];
+for( let {
+    name: n,
+    family: {father: f}
+} of people) {
+    console.log('Name: ' + n + 'Father: ' + f)
+    // Name: Mike SmithFather: Harry Smith
+    // Name: Tom JonesFather: Richard Jones
+}
