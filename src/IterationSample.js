8b75238d78d98b208265108cd1ee8fa823961dbe
gonnichiwa <keepthefaith00@gmail.com>
2024-05-28 11:33:38 +0900

    list insert, delete, useRef이용한 focus

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 const IterationSample = () => {
-    const names = [1,2,3,4,5];
-    const nameList = names.map((name, index) => <li key={index}>{name}</li>)
+    const [names, setNames] = useState([
+        {id: 1, text: 'a'},
+        {id: 2, text: 'b'},
+        {id: 3, text: 'c'},
+        {id: 4, text: 'd'}
+    ]);
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
+
+    const onChange = (e) => {
+        setInputText(e.target.value);
+    }
+
+    const inputRef = useRef();
+
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+        inputRef.current.focus();
+    }
+
+    const onRemove = (id) => {
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    }
+
+    const nameList = names.map(name => (
+        <li onDoubleClick={() => onRemove(name.id)} key={name.id}>{name.text}</li>
+    ));
     return (
         <div>
-            <ul>{nameList}</ul>
+          <input 
+            ref={inputRef}
+            type='text' 
+            value={inputText} 
+            onChange={onChange}/>
+          <button onClick={onClick}>추가</button>
+          <ul>{nameList}</ul>
         </div>
     );
 };
