bee6dafb9e4469d214da65d539a9404d79bcdc4b
gonnichiwa <keepthefaith00@gmail.com>
2024-05-30 10:25:43 +0900

    hook: useEffect

--- src/TextBoxUseState.js
+++ src/TextBoxUseState.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const TextBoxUseState = () => {
     const [state, setState] = useState({
         name: '',
         nickname: ''
     });
+    useEffect(() => {
+        console.log('렌더링이 완료되었습니다');
+        console.log(state);
+    }); // componentDidMount + componentDidUpdate
+
     const onChange = (e) => {
         setState({
             ...state,
@@ -15,8 +20,8 @@ const TextBoxUseState = () => {
     return (
         <>
         <div>
-            <h1>{state.name}</h1>
-            <h1>{state.nickname}</h1>
+            <h1>이름:{state.name}</h1>
+            <h1>닉네임:{state.nickname}</h1>
         </div>
         <div>
             <input type='text' name='name' placeholder='name' onChange={onChange}/>
