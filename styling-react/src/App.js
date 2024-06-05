4cd85a88558d1fd58d6f2f8edcbbafb1862553f2
gonnichiwa <keepthefaith00@gmail.com>
2024-06-05 11:23:06 +0900

    .sass media query 넣어서 적용 시험

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import MyCssModule from './MyCssModule';
 import SassComponent from './SassComponent';
 
 function App() {
   return (
     <div>
       <SassComponent/>
+      <MyCssModule/>
     </div>
   );
 }
