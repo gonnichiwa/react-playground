cc29044951c687a293d05325aa9a93a1f25516cd
gonnichiwa <keepthefaith00@gmail.com>
2024-06-06 10:10:17 +0900

    css-in-js : styled-component로 처리

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import MyCssModule from './MyCssModule';
 import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
     <div>
       <SassComponent/>
       <MyCssModule inverted={true}/>
+      <StyledComponent/>
     </div>
   );
 }
