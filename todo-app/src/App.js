9d59d6d56deb7150e9b80ca84d99465280131e2f
gonnichiwa <keepthefaith00@gmail.com>
2024-06-10 20:01:15 +0900

    add TodoTemplate component

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
-  return <div>Todo App을 만들자</div>;
+  return <TodoTemplate>Todo 앱을 만들자!</TodoTemplate>
 }
 
 export default App;
