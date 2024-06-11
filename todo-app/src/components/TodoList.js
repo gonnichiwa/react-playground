daacbe9aa69137161dd7f514a0a66505803c666c
gonnichiwa <keepthefaith00@gmail.com>
2024-06-11 17:41:20 +0900

    react-virtualized 추가  - 2500개 row중 화면에 안보이는 것들은 렌더링 없이 크기만 차지하도록   - 어떻게? 각 항목의 실제 크기 px단위로 알아내기.   - react-virtualized에서 제공하는 List 컴포넌트 사용해서 Item컴포넌트 div로 감쌈.

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    ({index, key, style}) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem 
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        />
+      )
+    },[onRemove, onToggle, todos]
+  );
   return (
-    <div>
-      {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
-      ))}
-    </div>
+    <List 
+      className='TodoList'
+      width={512}  // 전체 크기 (px)
+      height={513} // 전체 높이 (px 대충 Item 갯수 10개 잡음)
+      rowCount={todos.length} // 전체 항목 개수
+      rowHeight={57} // 항목 높이 (px)
+      rowRenderer={rowRenderer} // 항목을 렌더링 할 때 쓰는 함수
+      list={todos} // 배열 데이터
+      style={{outline: 'none'}} // List 컴포넌트 기본 적용 outline 스타일 제거
+    />
   );
 };
 
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -7,16 +7,19 @@ import {
 import './TodoListItem.scss';
 import cn from 'classnames';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
     const { id, text, checked } = todo;
     return (
-        <div className='TodoListItem'>
+        // TodoListItem-virtualized : 컴포넌트들 사이에 테두리 쳐줌, 홀수,짝수번째 다른 배경 색상 넣기 위함
+        <div className='TodoListItem-virtualized' style={style}>
+            <div className='TodoListItem'>
             <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}> { /* true : checkbox checked */}
                 {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
                 <div className='text'>{text}</div>
             </div>
             <div className='remove' onClick={() => onRemove(id)}>
                 <MdRemoveCircleOutline />
+            </div>
             </div>
         </div>
     );
