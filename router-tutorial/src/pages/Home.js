a4e6a1aaa1b136929c545f7c2f318c6861c9a8c3
gonnichiwa <keepthefaith00@gmail.com>
2024-06-12 20:33:32 +0900

    react-router-dom 으로 href 처리

--- router-tutorial/src/pages/Home.js
+++ router-tutorial/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => { // 본 Home.js 파일이 pages/ 경로에 있는데,
                      // 꼭 pages/ 이름, 경로depth 아니어도 상관없음.
@@ -6,6 +7,20 @@ const Home = () => { // 본 Home.js 파일이 pages/ 경로에 있는데,
         <div>
             <h1>홈</h1>
             <p>가장 먼저 보여지는 페이지 입니다.</p>
+            <ul>
+                <li>
+                    <Link to="/about">소개</Link>
+                </li>
+                <li>
+                    <Link to="/profiles/phikal">phikal의 프로필</Link>
+                </li>
+                <li>
+                    <Link to="/profiles/jin">jin의 프로필</Link>
+                </li>
+                <li>
+                    <Link to="/profiles/void">존재하지 않는 프로필</Link>
+                </li>
+            </ul>
         </div>
     );
 };
