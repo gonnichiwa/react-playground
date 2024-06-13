d4d5c1cd051f3deb0937c4fb738a023316cfede8
gonnichiwa <keepthefaith00@gmail.com>
2024-06-13 21:54:34 +0900

    useNavigate() : 페이지 이동 처리

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -11,7 +11,7 @@ import Layout from './pages/Layout';
 function App() {
   return (
     <Routes>
-      <Route element={<Layout/>}>
+      <Route path='/' element={<Layout/>}>
         <Route index element={<Home/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/profiles/:username' element={<Profile/>}/> {/* /profile/:username 하면 */}
