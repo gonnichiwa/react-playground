d4f8048573d59448a4a2b76724ce1ad4800f3b4e
gonnichiwa <keepthefaith00@gmail.com>
2024-06-13 22:04:13 +0900

    NavLink 구현사항 공통화

--- router-tutorial/src/pages/Articles.js
+++ router-tutorial/src/pages/Articles.js
@@ -1,11 +1,6 @@
 import { Link, NavLink, Outlet } from "react-router-dom";
 
 const Articles = () => {
-    const activeStyle = {
-        color: 'green',
-        fontSize: 21,
-    }
-
     return (
         <div>
             <div>
@@ -13,19 +8,28 @@ const Articles = () => {
             </div>
             <div>
                 <ul>
-                    <li>
-                        <NavLink to="/articles/1" style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 1</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/articles/2" style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 2</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/articles/3" style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 3</NavLink>
-                    </li>
+                    <ActiveItem id={1}/>
+                    <ActiveItem id={2}/>
+                    <ActiveItem id={3}/>
                 </ul>
             </div>
         </div>
     );
 };
+
+const ActiveItem = ({id}) => {
+    const activeStyle = {
+        color: 'green',
+        fontSize: 21,
+    };
+
+    return (
+        <>
+            <li>
+                <NavLink to={`/articles/${id}`} style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 {id}</NavLink>
+            </li>
+        </>
+    );
+}
 
 export default Articles;
