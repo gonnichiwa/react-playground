7b9a47b6d6501f55444ee99cf469db3b91a504ed
gonnichiwa <keepthefaith00@gmail.com>
2024-06-13 20:42:28 +0900

    쿼리스트링 용례

--- router-tutorial/src/pages/About.js
+++ router-tutorial/src/pages/About.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const About = () => {
+    const location = useLocation();
+    // location은 현재 보고 있는 페이지 정보를 가진 객체.
+    // search : 쿼리스트링 url              /about?name=jin&id=sdflsndg
+    //                  location.search  ?name=jin&id=sdflsndg
+
     return (
         <div>
             <h1>소개</h1>
             <p>리액트 라우터를 사용해 보는 프로젝트 입니다.</p>
+            <p>쿼리스트링 {location.search}</p>
         </div>
     );
 };
