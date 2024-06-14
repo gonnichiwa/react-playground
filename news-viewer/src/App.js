9a1f80d235028e8f783068f09568ee25837dd8d2
gonnichiwa <keepthefaith00@gmail.com>
2024-06-14 16:46:04 +0900

    상단 카테고리 publish

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
-  return <NewsList/>
+  return (
+    <div>
+      <Categories/>
+      <NewsList/>
+    </div>
+    
+  );
 };
 
 export default App;
