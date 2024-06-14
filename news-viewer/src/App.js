9498b2a9aae9c6a8bfa756d3d1f8e18040ecf228
gonnichiwa <keepthefaith00@gmail.com>
2024-06-14 15:51:04 +0900

    async await사용

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -3,11 +3,14 @@ import axios from '../node_modules/axios/index';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => {
-          setData(response.data);
-        })
+  const onClick = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      setData(response.data);
+    } catch (e) {
+      alert('data load error');
+      console.log(e);
+    }
   }
   return (
     <div>
