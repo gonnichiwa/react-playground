a7ddb15bd92a283b30cf8330e45b9085252c30a9
gonnichiwa <keepthefaith00@gmail.com>
2024-06-14 15:58:35 +0900

    axios get newsapi with apikey

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [data, setData] = useState(null);
   const onClick = async () => {
     try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=51acdda84c6844b7addb39d8a98089c2')
       setData(response.data);
     } catch (e) {
       alert('data load error');
